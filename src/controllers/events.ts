90996ccfb3e56aab2e8cf3b7e8322a680df614ee
rosch-arthur <rosch.arthur@gmail.com>
2024-08-05 03:08:00 -0300

    feat

--- src/controllers/events.ts
+++ src/controllers/events.ts
@@ -14,7 +14,7 @@ const sendPageViewEvent = async (req: Request, res: Response) => {
   const userAgent = req.headers['user-agent'];
   const fbp = req.body.fbp;
   const fbc = req.body.fbc;
-
+  console.log(fbc, fbp, userIp, userAgent)
   const userData = new UserData()
     .setClientIpAddress(userIp)
     .setClientUserAgent(userAgent)
@@ -99,6 +99,220 @@ const sendClickEvent = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(500).json({ success: false, error });
   }
+};
+
+const sendLeadEvent = async (req: Request, res: Response) => {
+  const current_timestamp = Math.floor(new Date().getTime() / 1000);
+  const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+  const fbp = req.body.fbp;
+  const fbc = req.body.fbc;
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  console.log(name,email, phone)
+  const userData = new UserData()
+    .setClientIpAddress(userIp)
+    .setClientUserAgent(userAgent)
+    .setFbp(fbp)
+    .setFbc(fbc)
+    .setEmails([email]) // Assuming `setEmail` method is implemented
+    .setPhones([phone])
+    .setFirstName(name)
+    .setLastName(name) // Assuming `setPhone` method is implemented
+
+  const serverEvent = new ServerEvent()
+    .setEventName('Lead')
+    .setEventTime(current_timestamp)
+    .setUserData(userData)
+    .setEventSourceUrl(req.body.url || 'http://example.com')
+    .setActionSource('website');
+
+  const eventsData = [serverEvent];
+  const eventRequest = new EventRequest(access_token, pixel_id).setEvents(eventsData);
+
+  try {
+    const response = await eventRequest.execute();
+    res.status(200).json({ success: true, response });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error });
+  }
+};
+
+const sendContactEvent = async (req: Request, res: Response) => {
+  const current_timestamp = Math.floor(new Date().getTime() / 1000);
+  const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+  const fbp = req.body.fbp;
+  const fbc = req.body.fbc;
+
+
+  const userData = new UserData()
+    .setClientIpAddress(userIp)
+    .setClientUserAgent(userAgent)
+    .setFbp(fbp)
+    .setFbc(fbc)
+
+  const serverEvent = new ServerEvent()
+    .setEventName('Contact')
+    .setEventTime(current_timestamp)
+    .setUserData(userData)
+    .setEventSourceUrl(req.body.url || 'http://example.com')
+    .setActionSource('website');
+
+  const eventsData = [serverEvent];
+  const eventRequest = new EventRequest(access_token, pixel_id).setEvents(eventsData);
+
+  try {
+    const response = await eventRequest.execute();
+    res.status(200).json({ success: true, response });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+};
+
+const sendInitiateCheckoutEvent = async (req: Request, res: Response) => {
+  const current_timestamp = Math.floor(new Date().getTime() / 1000);
+  const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+  const fbp = req.body.fbp;
+  const fbc = req.body.fbc;
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+
+  const userData = new UserData()
+    .setClientIpAddress(userIp)
+    .setClientUserAgent(userAgent)
+    .setFbp(fbp)
+    .setFbc(fbc)   
+    .setEmails([email])
+    .setPhones([phone])
+    .setFirstName(name)
+    .setLastName(name)
+
+  const serverEvent = new ServerEvent()
+    .setEventName('InitiateCheckout')
+    .setEventTime(current_timestamp)
+    .setUserData(userData)
+    .setEventSourceUrl(req.body.url || 'http://example.com')
+    .setActionSource('website');
+
+  const eventsData = [serverEvent];
+  const eventRequest = new EventRequest(access_token, pixel_id).setEvents(eventsData);
+
+  try {
+    const response = await eventRequest.execute();
+    res.status(200).json({ success: true, response });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+};
+
+const sendPurchaseCompletedEvent = async (req: Request, res: Response) => {
+  const current_timestamp = Math.floor(new Date().getTime() / 1000);
+  const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+  const fbp = req.body.fbp;
+  const fbc = req.body.fbc;
+  const email = req.body.buyer.email;
+  const name = req.body.buyer.name;
+  const phone = req.body.buyer.checkout_phone;
+  const address = req.body.buyer.address;
+
+  const userData = new UserData()
+    .setClientIpAddress(userIp)
+    .setClientUserAgent(userAgent)
+    .setFbp(fbp)
+    .setFbc(fbc)
+    .setEmails([email])
+    .setPhones([phone])
+    .setFirstName(name)
+    .setLastName(name)
+    .setCity(address.city)
+    .setState(address.state)
+    .setCountry(address.country)
+    .setZip(address.zipcode);
+
+  const serverEvent = new ServerEvent()
+    .setEventName('Purchase')
+    .setEventTime(current_timestamp)
+    .setUserData(userData)
+    .setEventSourceUrl(req.body.url || 'http://example.com')
+    .setActionSource('website');
+
+  const eventsData = [serverEvent];
+  const eventRequest = new EventRequest(access_token, pixel_id).setEvents(eventsData);
+
+  try {
+    if(req.body.event === "PURCHASE_COMPLETE" || req.body.event === "PURCHASE_APPROVED") {
+      const response = await eventRequest.execute();
+      res.status(200).json({ success: true, response });
+    } else {
+      res.status(200).json({ success: true });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error });
+  }
+};
+
+const sendCartAbandonmentEvent = async (req: Request, res: Response) => {
+  const current_timestamp = Math.floor(new Date().getTime() / 1000);
+  const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+  const fbp = req.body.fbp;
+  const fbc = req.body.fbc;
+  const { buyer, checkout_country} = req.body
+   
+  const userData = new UserData()
+    .setClientIpAddress(userIp)
+    .setClientUserAgent(userAgent)
+    .setFbp(fbp)
+    .setFbc(fbc)
+    .setEmails([buyer.email])
+    .setPhones([buyer.phone])
+    .setFirstName(buyer.name)
+    .setLastName(buyer.name)
+
+  const serverEvent = new ServerEvent()
+    .setEventName('CartAbandonment')
+    .setEventTime(current_timestamp)
+    .setUserData(userData)
+    .setEventSourceUrl(req.body.url || 'http://example.com')
+    .setActionSource('website');
+
+  const eventsData = [serverEvent];
+  const eventRequest = new EventRequest(access_token, pixel_id).setEvents(eventsData);
+
+  try {
+    const response = await eventRequest.execute();
+    res.status(200).json({ success: true, response });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+};
+
+export const InitiateCheckoutEvent = async (req: Request, res: Response) => {
+  await sendInitiateCheckoutEvent(req, res);
+};
+
+export const PurchaseCompletedEvent = async (req: Request, res: Response) => {
+  await sendPurchaseCompletedEvent(req, res);
+};
+
+export const CartAbandonmentEvent = async (req: Request, res: Response) => {
+  await sendCartAbandonmentEvent(req, res);
+};
+
+export const ContactEvent = async (req: Request, res: Response) => {
+  await sendContactEvent(req, res);
+};
+
+export const LeadEvent = async (req: Request, res: Response) => {
+  await sendLeadEvent(req, res);
 };
 
 export const PageViewEvent = async (req: Request, res: Response) => {
--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,17 +1,16 @@
 import { Router } from 'express';
-import { PageViewEvent, ViewContentEvent, ClickEvent} from './events';
+import { PageViewEvent, ViewContentEvent, ClickEvent, LeadEvent, ContactEvent, InitiateCheckoutEvent, CartAbandonmentEvent, PurchaseCompletedEvent} from './events';
 
 const routes = Router();
 300
 // routes.post('/track/purchase', PurchaseEvent);
 routes.post('/track/pageview', PageViewEvent);
-
 routes.post('/track/viewcontent', ViewContentEvent);
 routes.post('/track/click', ClickEvent);
-// routes.post('/track/addtocart', AddToCartEvent);
-// routes.post('/track/initiatecheckout', InitiateCheckoutEvent);
-// routes.post('/track/addpaymentinfo', AddPaymentInfoEvent);
-// routes.post('/track/lead', LeadEvent);
-// routes.post('/track/submitapplication', SubmitApplicationEvent);
+routes.post('/track/lead', LeadEvent);
+routes.post('/track/contact', ContactEvent);
+routes.post('/track/initiatecheckout', InitiateCheckoutEvent);
+routes.post('/track/cartAbandonmentEvent', CartAbandonmentEvent);
+routes.post('/track/purchaseCompletedEvent', PurchaseCompletedEvent);
 
 export { routes };
